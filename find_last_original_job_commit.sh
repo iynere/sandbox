89fbccd43bcbcebed21abb0c4c5a67a69dbcbb90
iynere <iynere@users.noreply.github.com>
2018-11-15 17:23:26 -0500

    fix script

--- find_last_original_job_commit.sh
+++ find_last_original_job_commit.sh
@@ -3,15 +3,16 @@
 RETRY=true
 JOB_NUM=$CIRCLE_PREVIOUS_BUILD_NUM
 
-while [ echo $RETRY ]
+while [[ $(echo $RETRY) == true ]]
 do
-  JOB_OUTPUT=$(curl https://circleci.com/api/v1.1/project/github/$CIRCLE_PROJECT_USERNAME/$CIRCLE_JOB/$JOB_NUM)
-
-  if [[ echo $JOB_OUTPUT | grep '"retry_of" : null' ]]; then
+  if [[ $(curl https://circleci.com/api/v1.1/project/github/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/$JOB_NUM | grep '"retry_of" : null') ]]; then
   	RETRY=false
   else
+  	echo "$JOB_NUM was a retry of a previous job"
     JOB_NUM=$(( $JOB_NUM - 1 ))
   fi
 done
 
-LAST_PUSHED_COMMIT=$(curl https://circleci.com/api/v1.1/project/github/$CIRCLE_PROJECT_USERNAME/$CIRCLE_JOB/$JOB_NUM | grep '"commit" : ' | sed -E 's/"commit" ://' | sed -E 's/[[:punct:]]//g')
+LAST_PUSHED_COMMIT=$(curl https://circleci.com/api/v1.1/project/github/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/$JOB_NUM | grep '"commit" : ' | sed -E 's/"commit" ://' | sed -E 's/[[:punct:]]//g')
+
+echo $LAST_PUSHED_COMMIT
