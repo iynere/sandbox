c0f85c7846d3e7fe20fbf99aaf1ae7f344b3cace
iynere <iynere@users.noreply.github.com>
2018-11-20 20:27:29 -0500

    still works on old branch?

--- find_last_original_job_commit.sh
+++ find_last_original_job_commit.sh
@@ -2,11 +2,15 @@
 
 #!/bin/bash
 
+## VARS
+
 # this starts as false, set to true to exit `until` loop
 FOUND_BASE_COMPARE_COMMIT=false
 
 # start iteration from the job before $CIRCLE_BUILD_NUM
 JOB_NUM=$(( $CIRCLE_BUILD_NUM - 1 ))
+
+## UTILS
 
 # abstract this logic out, it gets reused a few times
 extract_commit_from_job () {
@@ -16,7 +20,6 @@ extract_commit_from_job () {
   https://circleci.com/api/v1.1/project/$1/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/$2 | \
   grep '"vcs_revision" : ' | sed -E 's/"vcs_revision" ://' | sed -E 's/[[:punct:]]//g' | sed -E 's/ //g'
 }
-
 
 check_if_branch_is_new () {
   # takes a single argument for VCS_TYPE
@@ -42,20 +45,32 @@ check_if_branch_is_new () {
     fi
   done < API_ENDPOINTS_FOR_JOBS_ON_BRANCH
 
+  # clean up
   rm -f API_ENDPOINTS_FOR_JOBS_ON_BRANCH
 
+  # true or false
   echo $BRANCH_IS_NEW
 }
 
-# figure this out up top, so we don't worry about it later
+## SETUP
+
+# determine VCS type, so we don't worry about it later
 if [[ $(echo $CIRCLE_REPOSITORY_URL | grep github.com:$CIRCLE_PROJECT_USERNAME) ]]; then
   VCS_TYPE=github
 else
   VCS_TYPE=bitbucket
 fi
 
+# check if this is a new branch, as that informs later steps
+if [[ $(check_if_branch_is_new $VCS_TYPE) == true ]]; then
+  echo "$CIRCLE_SHA1 is the only commit on branch $CIRCLE_BRANCH"
+  BRANCH_IS_NEW=true
+else
+  BRANCH_IS_NEW=false
+fi
+
 # manually iterate downard through previous jobs
-until [[ $(echo $FOUND_BASE_COMPARE_COMMIT) == true ]]
+until [[ $FOUND_BASE_COMPARE_COMMIT == true ]]
 do
 
   # save circle api output to a temp file for reuse
@@ -76,22 +91,19 @@ do
   # edge cases:
   # 1. if $CIRCLE_SHA1 is the first commit on a new branch
     # then we need the most recent ancestor, branch-agnostic
-
-  # skip conditions 1 & 2/2.5:
-  if [[ $(grep "\"workflow_id\" : \"$CIRCLE_WORKFLOW_ID\"" JOB_OUTPUT) || \
-    ! $(grep '"retry_of" : null' JOB_OUTPUT) || \
-    $(grep "\"vcs_revision\" : \"$CIRCLE_SHA1\"" JOB_OUTPUT) ]]; then
-    echo "$JOB_NUM was a retry of a previous job, part of a rerun workflow, or else part of the current workflow"
-    JOB_NUM=$(( $JOB_NUM - 1 ))
-    continue
-  fi
+    # 1.5 a new branch doesn't always mean a new commit
 
   # handling condition 3 & edge case 1:
   # check if this is a brand-new branch
-  if [[ $(check_if_branch_is_new $VCS_TYPE) == true ]]; then
-    echo "$CIRCLE_SHA1 is the first commit on branch $CIRCLE_BRANCH"
-
+  if [[ BRANCH_IS_NEW == true ]]; then
     COMMIT_FROM_JOB_NUM=$(extract_commit_from_job $VCS_TYPE $JOB_NUM)
+
+    # we do a similar check later on, but it needs to be here too
+    # for edge case 1.5: an existing commit pushed to a new branch
+    if [[ $COMMIT_FROM_JOB_NUM == $CIRCLE_SHA1 ]]; then
+      JOB_NUM=$(( $JOB_NUM - 1 ))
+      continue
+    fi
 
     cd ~/project # <<parameters.project-path>>
 
@@ -104,21 +116,34 @@ do
       continue
     elif [[ $RETURN_CODE == 0 ]]; then
       FOUND_BASE_COMPARE_COMMIT=true
+      break
     else
       echo "unknown return code $RETURN_CODE from git merge-base with base commit $COMMIT_FROM_JOB_NUM, from job $JOB_NUM"
+      exit 1
     fi
   else
     # if not a new branch, find its most recent previous commit
-    if [[ $(grep "\"branch\" : \"$CIRCLE_BRANCH\"" JOB_OUTPUT) ]]; then
+
+    # by now, if none of conditions 1, 2/2.5, or 3 apply, we're done:
+    # 1. make sure job isn't part of the same workflow
+    if [[ ! $(grep "\"workflow_id\" : \"$CIRCLE_WORKFLOW_ID\"" JOB_OUTPUT) && \
+      # 2. make sure job is not a retry of a previous job
+      $(grep '"retry_of" : null' JOB_OUTPUT) && \
+      # 2.5 make sure job is not from a rerun workflow (same commit)
+      ! $(grep "\"vcs_revision\" : \"$CIRCLE_SHA1\"" JOB_OUTPUT) && \
+      # make sure we are on the same branch as $CIRCLE_BRANCH
+      # (we've already ruled out that this is a brand-new branch)
+      $(grep "\"branch\" : \"$CIRCLE_BRANCH\"" JOB_OUTPUT) ]]; then
       FOUND_BASE_COMPARE_COMMIT=true
     else
-      echo "$JOB_NUM was not on branch $CIRCLE_BRANCH"
+      echo "$JOB_NUM was part of the current workflow, part of a rerun workflow, a retry of a previous job, or from a different branch"
       JOB_NUM=$(( $JOB_NUM - 1 ))
       continue
     fi
   fi
 done
 
+# clean up
 rm -f JOB_OUTPUT
 
 BASE_COMPARE_COMMIT=$(extract_commit_from_job $VCS_TYPE $JOB_NUM)
